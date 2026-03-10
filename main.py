9e0befaa7e2d903d08d5458455a8fc988c29b868
lavosami <paramonovjegor@yandex.ru>
2026-03-10 04:25:04 +0300

    made changes

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+import uvicorn
+from app.api.routes.type_parameters import router as type_parameters_router
+from app.api.routes.analysis import router as analysis_router
 
-app = FastAPI()
+app = FastAPI(title="Anasti Analize Service", version="1.0.0")
+
+app.include_router(type_parameters_router)
+app.include_router(analysis_router)
 
 @app.get("/")
 async def root():
     return {"message": "Data Analize Service is running"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
